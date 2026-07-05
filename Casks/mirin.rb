82d6aa779d2543251308320672ef98fdaf0104e1
Ganesh M <ganeshm24122006@gmail.com>
2026-07-05 15:22:33 +0530

    Update Cask to v0.1.2 with proper checksums and architectures

--- Casks/mirin.rb
+++ Casks/mirin.rb
@@ -1,8 +1,14 @@
 cask "mirin" do
-  version "0.1.1"
-  sha256 :no_check # Replace with the actual SHA256 hash of the DMG file once GitHub Release builds it
+  version "0.1.2"
 
-  url "https://github.com/ganeshmshetty/mirin/releases/download/v#{version}/Mirin_#{version}_aarch64.dmg"
+  if Hardware::CPU.intel?
+    sha256 "4bf4dfb3c61c02c0635e08b5b168d717603f751143cd315c81cff7962a076205"
+    url "https://github.com/ganeshmshetty/mirin/releases/download/v#{version}/Mirin_#{version}_x64.dmg"
+  else
+    sha256 "f8bd2a855b7e60af0442a460c7f3a899dcce73a23a35a0ddff4e1f6152c6c3d5"
+    url "https://github.com/ganeshmshetty/mirin/releases/download/v#{version}/Mirin_#{version}_aarch64.dmg"
+  end
+
   name "Mirin"
   desc "Modern intuitive screen mirroring interface for Android (scrcpy)"
   homepage "https://github.com/ganeshmshetty/mirin"
