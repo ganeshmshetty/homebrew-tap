4623c060e936c5097c2487891f446e11d5dfb542
Ganesh M <ganeshm24122006@gmail.com>
2026-07-11 08:49:29 +0530

    feat: add RemoteCtrl cask and unify auto-update workflows

--- Casks/mirin.rb
+++ Casks/mirin.rb
@@ -2,10 +2,10 @@ cask "mirin" do
   version "0.1.2"
 
   if Hardware::CPU.intel?
-    sha256 "4bf4dfb3c61c02c0635e08b5b168d717603f751143cd315c81cff7962a076205"
+    sha256 :no_check
     url "https://github.com/ganeshmshetty/mirin/releases/download/v#{version}/Mirin_#{version}_x64.dmg"
   else
-    sha256 "f8bd2a855b7e60af0442a460c7f3a899dcce73a23a35a0ddff4e1f6152c6c3d5"
+    sha256 :no_check
     url "https://github.com/ganeshmshetty/mirin/releases/download/v#{version}/Mirin_#{version}_aarch64.dmg"
   end
 
---
+++ Casks/remotectrl.rb
@@ -0,0 +1,25 @@
+cask "remotectrl" do
+  version "0.0.4"
+
+  if Hardware::CPU.intel?
+    sha256 :no_check
+    url "https://github.com/ganeshmshetty/RemCtrl/releases/download/v#{version}/RemoteCtrl-#{version}.dmg"
+  else
+    sha256 :no_check
+    url "https://github.com/ganeshmshetty/RemCtrl/releases/download/v#{version}/RemoteCtrl-#{version}-arm64.dmg"
+  end
+
+  name "RemoteCtrl"
+  desc "Remote browser control desktop app"
+  homepage "https://github.com/ganeshmshetty/RemCtrl"
+
+  app "RemoteCtrl.app"
+
+  zap trash: [
+    "~/Library/Application Support/RemoteCtrl",
+    "~/Library/Application Support/com.RemoteCtrl.app",
+    "~/Library/Preferences/com.RemoteCtrl.app.plist",
+    "~/Library/Saved Application State/com.RemoteCtrl.app.savedState",
+    "~/.config/RemoteCtrl"
+  ]
+end
