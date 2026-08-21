506965533356518978b4624fa94770ef95d97ab9
Ganesh M <ganeshm24122006@gmail.com>
2026-08-21 10:07:15 +0530

    openclip 1.0.0

--- Casks/openclip.rb
+++ Casks/openclip.rb
@@ -1,6 +1,6 @@
 cask "openclip" do
   version "1.0.0"
-  sha256 "88662620da6d2b79eaed8c3c4c52ddc7c11c4bc62f2e3e9c3268cd76b6023a33"
+  sha256 "7ee0cb794b919f80ea8236a5b9a52a72b8d32ab9a6ece332ca319e2a9ca5ca09"
 
   url "https://github.com/ganeshmshetty/openclip/releases/download/v#{version}/OpenClip-v#{version}.zip"
   name "OpenClip"
