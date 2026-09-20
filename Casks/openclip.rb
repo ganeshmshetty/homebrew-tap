06570f3c29c680c54081b1837dd28cd907819e6b
Ganesh M <ganeshm24122006@gmail.com>
2026-09-20 11:43:13 +0530

    fix(openclip): remove quarantine workaround and caveats for signed binary

--- Casks/openclip.rb
+++ Casks/openclip.rb
@@ -4,7 +4,7 @@ cask "openclip" do
 
   url "https://github.com/ganeshmshetty/openclip/releases/download/v#{version}/OpenClip-v#{version}.zip"
   name "OpenClip"
-  desc "Instant actions for selected text on macOS"
+  desc "Instant actions for selected text"
   homepage "https://www.getopenclip.app/"
 
   auto_updates true
@@ -12,23 +12,10 @@ cask "openclip" do
 
   app "OpenClip.app"
 
-  # ad-hoc signed, no Apple Developer ID yet - strip quarantine for 0 friction
-  # TODO: remove postflight after Developer ID/notarization
-  postflight do
-    system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "#{appdir}/OpenClip.app"]
-  end
-
   zap trash: [
     "~/.openclip",
     "~/Library/Caches/com.openclip.OpenClip",
     "~/Library/Logs/OpenClip",
     "~/Library/Preferences/com.openclip.OpenClip.plist",
   ]
-
-  caveats do
-    <<~EOS
-      OpenClip is ad-hoc signed (no Apple Developer ID). Gatekeeper quarantine is auto-removed
-      by postflight (xattr -dr). Verify: codesign -dv /Applications/OpenClip.app
-    EOS
-  end
 end
